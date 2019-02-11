7ab0d67e5e58ad3b3917c17a425c1a54a9e9708c
Tom929 <tom.resink@yahoo.com>
2019-02-11 23:15:42 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,6 +21,9 @@ from properties import negative
     #x-axis values for pH plot, (decrease last number for higher resolution)
 x = np.arange(0,14.25,0.25)
 
+    #n refers to figure number
+n = 1
+
 #classes (where the magic happens)
 
     #attributes of the inputted polypeptide seqeuence
@@ -77,72 +80,71 @@ def inputnumber():
     return
 
 def generatechargeplot():
-    for i in range(0,int(len(sequences))):
-        sequences[i].chargeplot()
+    global n
+    chargeplot = plt.figure(n) 
+    for i in range(0,int(len(sequences))): #generates the y values
+        sequences[i].chargeplot() 
+
+    plt.grid() # adds gridlines
+
+    plt.xlabel('pH') 
+    plt.ylabel('Relative Charge')
+    plt.legend() 
+
+    #configure axes
+    ax = plt.gca() #get current axes (then spines to configure axes)
+    ax.spines['top'].set_color('none') #removes top black line on plot
+    ax.spines['bottom'].set_position('zero') #ensures x axis always crosses at y = 0
+    ax.spines['right'].set_color('none') #removes right black line on plot
+
+    #x axis ticks  
+    start,end = ax.get_xlim() #gets min and max of x axis
+    end = end+1
+    ax.set_xticks(np.arange(0,round(end),1)) #sets major ticks on x axis
+    ax.set_xticks(np.arange(round(start),round(end),0.25),True) #sets minor ticks on x axis
+
+    #y axis ticks 
+    start,end = ax.get_ylim() #gets min and max of y axis
+    end = end + 1
+    ax.set_yticks(np.arange(round(start),round(end),1),True) #sets minor ticks on y axis
+    
+    n = n + 1 #keeps figure numbers in check
     return
 
 def generatemassplot():
-    for i in range(0,int(len(sequences))):
+    global n
+    massplot = plt.figure(n)
+    for i in range(0,int(len(sequences))): #generates the y values
         sequences[i].massplot()
+
+    plt.xlabel('Protein')
+    plt.ylabel('Mass (kDa)')
+    plt.legend()
+
+    #configure axes
+    bx = plt.gca() #get current axes (then spines to configure axes)
+    bx.spines['top'].set_color('none') #removes top black line on plot
+    bx.spines['bottom'].set_position('zero') #ensures x axis always crosses at y = 0
+    bx.spines['right'].set_color('none') #removes right black line on plot
+    bx.set_axisbelow(True)
+
+    #y axis ticks 
+    start,end = bx.get_ylim() #gets min and max of y axis
+    start = 0
+    end = end + 1
+    bx.set_yticks(np.arange(0,round(end),5)) #sets major ticks on x axis
+    bx.set_yticks(np.arange(round(start),round(end),1),True) #sets minor ticks on y axis
+    bx.yaxis.grid(which='major')
+
+    n = n + 1 #keeps figure numbers in check
     return
 
 
 #interface
 inputnumber() # user input of protein sequences
 
-fig = plt.figure(1) # makes figure 1 (size and chargeplot)
-
-fig.add_subplot(211) # makes subplot 1 (chargeplot)
-plt.grid() # adds gridlines
-
-generatechargeplot() # generate charge plot (duh!)
-
-plt.xlabel('pH') 
-plt.ylabel('Relative Charge')
-plt.legend() 
-
-    #configure axes
-ax = plt.gca() #get current axes (then spines to configure axes)
-ax.spines['top'].set_color('none') #removes top black line on plot
-ax.spines['bottom'].set_position('zero') #ensures x axis always crosses at y = 0
-ax.spines['right'].set_color('none') #removes right black line on plot
-
-    #x axis ticks  
-start,end = ax.get_xlim() #gets min and max of x axis
-end = end+1
-ax.set_xticks(np.arange(0,round(end),1)) #sets major ticks on x axis
-ax.set_xticks(np.arange(round(start),round(end),0.25),True) #sets minor ticks on x axis
-
-    #y axis ticks 
-start,end = ax.get_ylim() #gets min and max of y axis
-end = end + 1
-ax.set_yticks(np.arange(round(start),round(end),1),True) #sets minor ticks on y axis
-
-
-fig.add_subplot(212) # makes subplot 2 (sizeplot)
-
-
+generatechargeplot()
 generatemassplot()
-
-plt.xlabel('Protein')
-plt.ylabel('Mass (kDa)')
-plt.legend()
-
-    #configure axes
-bx = plt.gca() #get current axes (then spines to configure axes)
-bx.spines['top'].set_color('none') #removes top black line on plot
-bx.spines['bottom'].set_position('zero') #ensures x axis always crosses at y = 0
-bx.spines['right'].set_color('none') #removes right black line on plot
-bx.set_axisbelow(True)
-
-#y axis ticks 
-start,end = bx.get_ylim() #gets min and max of y axis
-start = 0
-end = end + 1
-bx.set_yticks(np.arange(0,round(end),5)) #sets major ticks on x axis
-bx.set_yticks(np.arange(round(start),round(end),1),True) #sets minor ticks on y axis
-bx.yaxis.grid(which='major')
-
 
 plt.show()
 
