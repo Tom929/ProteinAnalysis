1df28f9d32cbc94ae84c59b4d5e13f519d848566
Tom929 <tom.resink@yahoo.com>
2019-02-16 00:53:34 +0000

    functioning database access

--- Main.py
+++ Main.py
@@ -20,6 +20,9 @@ from properties import NEGATIVE
     #database access
 import database as data
 
+   #figure plotting
+import graph
+
 #classes (where the magic happens)
 
     #attributes of the inputted polypeptide seqeuence
@@ -40,6 +43,7 @@ class protein:
 
         #calculates relative charge on peptide at specific pH
     def chargeplot(self):
+        x = np.arange(0,14.25,0.25) #x values for plot and calculation of charge
         temp = 0 #introduce temporary variable to add up equation for charges
         for i in range(int(len(self.sequence))): #calculate the charge on the radicals
             if self.sequence[i] in prop.POSITIVE:
@@ -63,7 +67,7 @@ class protein:
         for i in range(int(len(self.sequence))): #add up the masses of the residues in Da
             temp = temp + ref.MASS[self.sequence[i]]
         temp = temp + 16 #include the extra oxygen present in the last carboxy group
-        self.mass = int(temp)/1000 #conversion to kDA
+        self.mass = int(temp)/1000 #conversion to kDA (conversion to int gives 3 d.p.)
         return plt.bar(self.name,self.mass,label=str(self.mass) + ' kDA') #returns plot of kDA
 
 #functions (mainly for interfacing)
@@ -73,66 +77,12 @@ def inputnumber():
     sequences = list()
     number = int(input('How many sequences would you like to input?\n>>>'))
     for i in range(0,number):
-        #sequences.append(protein(input('name of protein ' + str(i+1) + ':'),input('sequence of protein ' + str(i+1) + ':'))) #adds proteins to sequences list
-        sequences.append(data.access_sequence(input('Protein ' + str(i+1)+ ': '))) #list of sequences accessed at SwissProt
+        temp = input('Protein ' + str(i+1)+ ': ')
+        sequences.append(protein(data.access_name(temp), data.access_sequence(temp))) #list of sequences accessed at SwissProt
+        print(data.access_sequence(temp))
     return
 
-def generatechargeplot():
-    chargeplot = plt.figure()
-    x = np.arange(0,14.25,0.25) 
-    for i in range(0,int(len(sequences))): #generates the y values
-        sequences[i].chargeplot() 
 
-    plt.grid() # adds gridlines
-
-    plt.xlabel('pH') 
-    plt.ylabel('Relative Charge')
-    plt.legend() 
-
-    #configure axes
-    ax = plt.gca() #get current axes (then spines to configure axes)
-    ax.spines['top'].set_color('none') #removes top black line on plot
-    ax.spines['bottom'].set_position('zero') #ensures x axis always crosses at y = 0
-    ax.spines['right'].set_color('none') #removes right black line on plot
-
-    #x axis ticks  
-    start,end = ax.get_xlim() #gets min and max of x axis
-    end = end+1
-    ax.set_xticks(np.arange(0,round(end),1)) #sets major ticks on x axis
-    ax.set_xticks(np.arange(round(start),round(end),0.25),True) #sets minor ticks on x axis
-
-    #y axis ticks 
-    start,end = ax.get_ylim() #gets min and max of y axis
-    end = end + 1
-    ax.set_yticks(np.arange(round(start),round(end),1),True) #sets minor ticks on y axis
-    
-    return chargeplot 
-
-def generatemassplot():
-    massplot = plt.figure()
-    for i in range(0,int(len(sequences))): #generates the y values
-        sequences[i].massplot()
-
-    plt.xlabel('Protein')
-    plt.ylabel('Mass (kDa)')
-    plt.legend()
-
-    #configure axes
-    bx = plt.gca() #get current axes (then spines to configure axes)
-    bx.spines['top'].set_color('none') #removes top black line on plot
-    bx.spines['bottom'].set_position('zero') #ensures x axis always crosses at y = 0
-    bx.spines['right'].set_color('none') #removes right black line on plot
-    bx.set_axisbelow(True)
-
-    #y axis ticks 
-    start,end = bx.get_ylim() #gets min and max of y axis
-    start = 0
-    end = end + 1
-    bx.set_yticks(np.arange(0,round(end),5)) #sets major ticks on x axis
-    bx.set_yticks(np.arange(round(start),round(end),1),True) #sets minor ticks on y axis
-    bx.yaxis.grid(which='major')
-
-    return massplot
 
 def filecreate(name):
     file = open(name,'w')
@@ -149,31 +99,11 @@ def filecreate(name):
 #interface
 inputnumber() # user input of protein sequences
 
-print(sequences)
+#print(len(sequences))
 
-#filecreate('current')
+#filecreate('current') ## in progress to write text file with current inputted sequences
 
-#generatechargeplot()
-#generatemassplot()
+graph.generatechargeplot(sequences)
+graph.generatemassplot(sequences)
 
-
-
-#plt.show()
-
-
-
-
-
-#Some sequences to test:
-
-    #EGF
-#NSDSECPLSHDGYCLHDGVCMYIEALDKYACNCVVGYIGERCQYRDLKWWELR
-
-    #Insulin
-#MALWMRLLPLLALLALWGPDPAAAFVNQHLCGSHLVEALYLVCGERGFFYTPKTRREAEDLQGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCN
-
-    #FGF2
-#MAAGSITTLPALPEDGGSGAFPPGHFKDPKRLYCKNGGFFLRIHPDGRVDGVREKSDPHIKLQLQAEERGVVSIKGVCANRYLAMKEDGRLLASKCVTDECFFFERLESNNYNTYRSRKYTSWYVALKRTGQYKLGSKTGPGQKAILFLPMSAKS
-
-    #TGFB1
-#MPPSGLRLLLLLLPLLWLLVLTPGRPAAGLSTCKTIDMELVKRKRIEAIRGQILSKLRLASPPSQGEVPPGPLPEAVLALYNSTRDRVAGESAEPEPEPEADYYAKEVTRVLMVETHN
+plt.show()
